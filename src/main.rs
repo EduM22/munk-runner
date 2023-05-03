108b345c2188ce15750c641fc8ac3cef4557d27e
EduM22 <38257387+EduM22@users.noreply.github.com>
2023-05-03 21:17:40 +0200

    add 404 fallback

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use axum::{response::Html, routing::get, Router};
-use std::net::{SocketAddr, IpAddr, Ipv6Addr};
+use axum::{http::StatusCode, response::Html, routing::get, Router};
+use std::net::{IpAddr, Ipv6Addr, SocketAddr};
 
 #[tokio::main]
 async fn main() {
     // build our application with a route
-    let app = Router::new().route("/", get(handler));
-
+    let app = Router::new().route("/", get(handler)).fallback(fallback);
     // run it
     let addr = &SocketAddr::new(IpAddr::from(Ipv6Addr::UNSPECIFIED), 8080);
     println!("listening on {}", addr);
@@ -17,4 +16,8 @@ async fn main() {
 
 async fn handler() -> Html<&'static str> {
     Html("<h1>Hello, World!</h1>")
+}
+
+async fn fallback() -> (StatusCode, &'static str) {
+    (StatusCode::NOT_FOUND, "Not Found")
 }
